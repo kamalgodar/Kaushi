4bf76a8b0a8ef5666ed62567e131557d66fda161
kamalgodar <kamalgodar9244@gmail.com>
2020-12-01 23:57:42 +0545

    shop

--- main/views.py
+++ main/views.py
@@ -120,7 +120,55 @@ def market(request):
 
     
 def shop(request):
-    return render(request, 'shop.html', {})
+    
+    product1 = Product()
+    product1.name = 'Apple'
+    product1.price = 110
+    product1.img = 'carrot.jpg'
+
+    product2 = Product()
+    product2.name = 'Cauliflower'
+    product2.price = 40
+    product2.img = 'cauliflower.jpg'
+
+    product3 = Product()
+    product3.name = 'Dustpan'
+    product3.price = 200
+    product3.img = 'dustpan.jpg'
+
+    product4 = Product()
+    product4.name = 'Carrot'
+    product4.price = 30
+    product4.img = 'carrot.jpg'
+
+    product5 = Product()
+    product5.name = 'Sprayer'
+    product5.price = 120
+    product5.img = 'sprayer.jpg'
+
+    product6 = Product()
+    product6.name = 'Gloves'
+    product6.price = 80
+    product6.img = 'gloves.jpg'
+
+    product7 = Product()
+    product7.name = 'Compost Manure'
+    product7.price = 40
+    product7.img = 'compostmanure.jpg'
+
+    product8 = Product()
+    product8.name = 'Spade'
+    product8.price = 150
+    product8.img = 'spade.jpg'
+
+    product9 = Product()
+    product9.name = 'Cabbahge'
+    product9.price = 40
+    product9.img = 'carrot.jpg'
+
+    products = [product1, product2, product3, product4, product5, product6, product7, product8, product9]
+
+    return render(request, 'shop.html', {'products': products})
 
 def manpower(request):
     return render(request, 'manpower.html', {})
